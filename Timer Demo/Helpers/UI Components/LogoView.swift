4440cf397c5dede8350df8533add2f945145013a
jay-jay-lama <alina.petrovskaia92@gmail.com>
2024-12-19 13:50:51 +0100

    1

--- Timer Demo/Helpers/UI Components/LogoView.swift
+++ Timer Demo/Helpers/UI Components/LogoView.swift
@@ -21,6 +21,7 @@ struct LogoView: View {
                 .frame(140)
                 .rotationEffect(.degrees(degree))
                 .onAppear(perform: onAppear)
+                .onChange(of: isAnimating) { onAnimatingStatusChanged($1) }
         }
     }
 }
@@ -32,11 +33,29 @@ private extension LogoView {
             .start()
     }
     func onDisappear() {
-        isAnimating = false
         animationTimer.cancel()
+    }
+    func onAnimatingStatusChanged(_ value: Bool) {
+        switch value {
+            case true: try? animationTimer.start()
+            case false: onAnimationStop()
+        }
     }
     func onTimerCall(_ time: MTime) {
         guard isAnimating else { return }
-        withAnimation { degree += 3 }
+        withAnimation { degree -= 3 }
     }
+    func onAnimationStop() {
+        animationTimer.cancel()
+        withAnimation(.interpolatingSpring(duration: 1, bounce: 0, initialVelocity: 0)) {
+            guard Int(degree) % 90 != 0 else { return }
+            let numberOfCircles = Int(abs(degree) / 90) + 1
+            degree = -Double(numberOfCircles * 90)
+        }
+    }
+}
+
+#Preview {
+    try? MTimer(.breakfast).publish(every: 1).start()
+    return TimerDetailView(.breakfast)
 }
